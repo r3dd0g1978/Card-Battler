50873806889ad35d033c8441a3fa0ef89114e3e2
r3dd0g1978 <130005368+r3dd0g1978@users.noreply.github.com>
2023-12-06 20:50:29 +1100

    Scriptable Objects

--- Card Battler/Assets/Scripts/Card.cs
+++ Card Battler/Assets/Scripts/Card.cs
@@ -2,25 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    public CardScriptableObject cardSO;
+    
     public int currentHealth, attackPower, manaCost;
 
-    public TMP_Text healthText, attackText, costText;
-    
-    // Start is called before the first frame update
+    public TMP_Text healthText, attackText, costText, nameText, actionDescriptionText, loreText;
+
+    public Image characterArt, bgArt;
+
+
     void Start()
     {
+        SetupCard();
+    }
+
+    void Update()
+    {
+    }
+
+    public void SetupCard()
+    {
+        currentHealth = cardSO.currentHealth;
+        attackPower = cardSO.attackPower;
+        manaCost = cardSO.manaCost;       
+        
         healthText.text = currentHealth.ToString();
         attackText.text = attackPower.ToString();
         costText.text = manaCost.ToString();
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        nameText.text = cardSO.cardName;
+        actionDescriptionText.text = cardSO.actionDescription;
+        loreText.text = cardSO.cardLore;
+
+        characterArt.sprite = cardSO.characterSprite;
+        bgArt.sprite = cardSO.bgSprite;
     }
 }
