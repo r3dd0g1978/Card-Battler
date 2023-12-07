2bf38a7d6161732ec5bcd2cef2d25bc7b4d4ed66
r3dd0g1978 <130005368+r3dd0g1978@users.noreply.github.com>
2023-12-07 20:42:39 +1100

    Hand Controller

---
+++ Card Battler/Assets/Scripts/HandController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+
+    public List<Card> heldCards = new List<Card>();
+
+    public Transform minPos, maxPos;
+
+    public List<Vector3> cardPositions = new List<Vector3>();
+
+    void Start()
+    {
+        SetCardPositionsInHand();
+    }
+
+    void Update()
+    {
+    }
+
+    public void SetCardPositionsInHand()
+    {
+        cardPositions.Clear();
+
+        Vector3 distanceBetweenPoints = Vector3.zero;
+
+        if (heldCards.Count > 1)
+        {
+            distanceBetweenPoints = (maxPos.position - minPos.position) / (heldCards.Count - 1);
+        }
+
+        for (int i = 0; i < heldCards.Count; i++)
+        {
+            cardPositions.Add(minPos.position + (distanceBetweenPoints * i));
+
+            heldCards[i].transform.position = cardPositions[i];
+        }
+    }
+}
